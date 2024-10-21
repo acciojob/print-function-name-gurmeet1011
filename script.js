f7da1d31b26ebf2495c1a51181b157622d313959
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-22 00:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function functionName() {
+    // Display the name of the function using an aler
+    alert(functionName.name);
+}
+
+// Calling the function to test it
+functionName(); // This will alert "functionName"
+// const functionName = function() {
+//     alert(arguments.callee.name);
+// };
+
+// // Calling the function to test it
+// functionName(); // This will alert "functionName"
